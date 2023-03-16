89c001f3a62b2c05b2ccdc9b4d6ddb512e2f5844
OezguerGenc <oezguer.eren.genc@gmail.com>
2023-03-16 04:00:34 +0100

    Created checkSavedQuestions method to loadquestionslist per API only when never loaded and saved localy to improve performance

--- lib/viewmodel/quizmodel.dart
+++ lib/viewmodel/quizmodel.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutterquizapp/model/answer.dart';
 import 'package:flutterquizapp/model/question.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class QuizModel {
   final databaseRef = FirebaseDatabase.instance.ref().child("questions");
@@ -8,49 +9,65 @@ class QuizModel {
   int questionindex = 0;
   int isSelectedIndex = 0;
 
-  Future<List<Question>> loadQuestions() async {
-    List<Question> questionlist = [];
-    for (var i = 0; i < 100; i++) {
-      if (await databaseRef
-              .child("$i/text")
-              .get()
-              .then((value) => value.value) !=
-          null) {
-        List<Answer> answers = [];
-        for (var j = 0; j < 4; j++) {
-          if (await databaseRef
-                  .child("$i/answer/$j/text")
-                  .get()
-                  .then((value) => value.value) !=
-              null) {
-            print(await databaseRef
-                .child("$i/answer/$j/text")
-                .get()
-                .then((value) => value.value.toString()));
-            answers.add(Answer(
-                await databaseRef
-                    .child("$i/answer/$j/correct")
-                    .get()
-                    .then((value) => value.value) as bool,
-                await databaseRef
-                    .child("$i/answer/$j/text")
-                    .get()
-                    .then((value) => value.value.toString())));
-          } else {
-            break;
-          }
-        }
-        questionlist.add(Question(
-            await databaseRef
+  Future<bool> checkSavedQuestions() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getStringList("questionlist") == null;
+  }
+
+  Future<List<Question>?> loadQuestions() async {
+    final prefs = await SharedPreferences.getInstance();
+    List<Question>? questionlist = [];
+
+    if (await checkSavedQuestions()) {
+      for (var i = 0; i < 100; i++) {
+        if (await databaseRef
                 .child("$i/text")
                 .get()
-                .then((value) => value.value.toString()),
-            answers));
-      } else {
-        break;
+                .then((value) => value.value) !=
+            null) {
+          List<Answer> answers = [];
+          for (var j = 0; j < 4; j++) {
+            if (await databaseRef
+                    .child("$i/answer/$j/text")
+                    .get()
+                    .then((value) => value.value) !=
+                null) {
+              print(await databaseRef
+                  .child("$i/answer/$j/text")
+                  .get()
+                  .then((value) => value.value.toString()));
+              answers.add(Answer(
+                  await databaseRef
+                      .child("$i/answer/$j/correct")
+                      .get()
+                      .then((value) => value.value) as bool,
+                  await databaseRef
+                      .child("$i/answer/$j/text")
+                      .get()
+                      .then((value) => value.value.toString())));
+            } else {
+              break;
+            }
+          }
+          questionlist.add(Question(
+              await databaseRef
+                  .child("$i/text")
+                  .get()
+                  .then((value) => value.value.toString()),
+              answers));
+        } else {
+          break;
+        }
       }
+
+      await prefs.setStringList(
+          "questionlist", questionlist.map((q) => q.toJson()).toList());
+    } else {
+      List<String>? questionStringList = prefs.getStringList('questionlist');
+      questionlist =
+          questionStringList?.map((q) => Question.fromJson(q)).toList();
     }
-    print(questionlist[0].answers.length);
 
     return questionlist;
   }
