c7e4d001a5c612dfe33b8065e46644c5dc279438
OezguerGenc <oezguer.eren.genc@gmail.com>
2023-03-23 03:57:06 +0100

    Created networkmodel with connectedWithNetwork method to check the connectivity

---
+++ lib/viewmodel/networkmodel.dart
@@ -0,0 +1,14 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+class NetworkModel {
+  Future<bool> connectedWithNetwork() async {
+    final connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.mobile) {
+      return true;
+    } else if (connectivityResult == ConnectivityResult.wifi) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
